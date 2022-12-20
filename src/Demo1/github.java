e5458702809b6219931cb4d2bfc911eca565e8b2
impat <pratimapawar7507@gmail.com>
2022-12-20 18:20:19 +0530

    new class

---
+++ src/Demo1/github.java
@@ -0,0 +1,10 @@
+package Demo1;
+
+public class github {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello world");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author impat
+ *
+ */
+module New1 {
+}
